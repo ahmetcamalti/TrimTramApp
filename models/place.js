3fc553d7db8c2399cc7b790bb0ea450a8b88db8c
canbax <yusufsaidcanbaz@gmail.com>
2017-03-25 11:32:37 +0300

    place model added

---
+++ models/place.js
@@ -0,0 +1,17 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var placeSchema = new Schema({
+  title: String,
+  lat:String,
+  long:String
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var Place = mongoose.model('Place', travelSchema);
+
+// make this available in our Node applications
+module.exports = Place;
