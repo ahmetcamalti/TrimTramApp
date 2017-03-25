704315e8412d8a81e5e40d35f3e2e0f46ddd95bc
ahmetcamalti <ahmetcamalti@gmail.com>
2017-03-25 04:49:17 +0000

    Implemented travel.js file and added to models folder.

---
+++ models/travel.js
@@ -0,0 +1,27 @@
+// grab the things we need
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+// create a schema
+var travelSchema = new Schema({
+  title: String,
+  time: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'time'
+  }],
+  places: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'place'
+  }],
+  users: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user'
+  }]
+});
+
+// the schema is useless so far
+// we need to create a model using it
+var Travel = mongoose.model('Travel', travelSchema);
+
+// make this available in our Node applications
+module.exports = Travel;
